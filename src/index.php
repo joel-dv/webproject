7619cbc26203afd8cc18487efa6ca3897d19bdcf
kddsgn <45744261+kddsgn@users.noreply.github.com>
2018-12-10 01:25:28 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -5,9 +5,9 @@
 	<link rel="stylesheet" type="text/css" href="css/main.min.css">
 </head>
 <body>
-<h1>Hello World...! or this is public? It's not! Maybe it is... I think it is:)</h1>
+<h1>Hello World! :)</h1>
 <hr>
 <p>Hey!</p>
-<button>I love u!</button>
+<button>Hey!</button>
 </body>
 </html>
